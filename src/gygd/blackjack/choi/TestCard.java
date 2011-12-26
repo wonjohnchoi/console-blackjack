7b7aede93af833d8755e79ee0b9014928d33f6f6
Wonjohn Choi <wonjohn.choi@gmail.com>
2011-12-26 00:01:45 -0800

    first commit

---
+++ src/gygd/blackjack/choi/TestCard.java
@@ -0,0 +1,22 @@
+package gygd.blackjack.choi;
+
+
+/**
+ * @author Wonjohn Choi
+ * @course ICS4U1
+ * @project Blackjack
+ * @description test program of the Card class
+ * @Date April 22, 2010
+ */
+public class TestCard {
+	public static void main(String args[])
+	{
+		//instantiate a card
+		Card card=new Card(10, "Diamond", "K");
+		
+		//test methods 
+		System.out.println("Value: "+card.getValue()); //value: 11
+		
+		System.out.println("Card: "+card); //Card: K Diamond
+	}
+}
